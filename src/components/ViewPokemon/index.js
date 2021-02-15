52e9de51dcad17665fe48787d6e376bfa072f439
kelvin diego <kelvin.fabiana@gmail.com>
2021-02-15 15:04:24 +0000

    div principal com 90%

--- src/components/ViewPokemon/index.js
+++ src/components/ViewPokemon/index.js
@@ -7,7 +7,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import CardMedia from "@material-ui/core/CardMedia";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Divider from "@material-ui/core/Divider";
 const useStyles = makeStyles((theme) => ({
   cardRoot: {
@@ -33,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
   titlePokemon: {
     alignSelf: "flex-end",
     flex: 1,
-    textAlign: 'center'
+    textAlign: "center",
   },
   pokemonAbilities: {
     marginTop: "5px",
@@ -43,59 +43,63 @@ const useStyles = makeStyles((theme) => ({
     flex: 1,
   },
   root: {
+    width:'90%',
     marginTop: "10px",
     display: "grid",
+    margin:' 0 auto'
   },
 }));
 function ViewPokemon({ pokemon }) {
   React.useEffect(() => {
     console.log(pokemon);
   }, [pokemon]);
-  const { abilities, caracteristicas,otherDetails,location } = pokemon;
+  const { abilities, caracteristicas, otherDetails, location } = pokemon;
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item sm={4}>
           <Card className={classes.cardRoot}>
-            {
-              otherDetails.sprites !== undefined &&
-              <CardMedia component="img"
-               image={otherDetails.sprites.other['official-artwork'].front_default}
+            {otherDetails.sprites !== undefined && (
+              <CardMedia
+                component="img"
+                image={
+                  otherDetails.sprites.other["official-artwork"].front_default
+                }
               />
-            }
+            )}
             <CardContent>
-              {
-                caracteristicas.name !== undefined &&
+              {caracteristicas.name !== undefined && (
                 <Typography
-                variant="h5"
-                component="h2"
-                display="block"
-                className={classes.titlePokemon}
-                >{caracteristicas.name}</Typography>
-              }
-
+                  variant="h5"
+                  component="h2"
+                  display="block"
+                  className={classes.titlePokemon}
+                >
+                  {caracteristicas.name}
+                </Typography>
+              )}
             </CardContent>
           </Card>
         </Grid>
         <Grid item sm={8}>
           <Typography
-            variant="h5"
-            component="h2"
+            variant="h3"
             display="block"
             className={classes.descriptionTitle}
           >
             Caracteristicas
           </Typography>
           {caracteristicas.flavor_text_entries !== undefined &&
-            caracteristicas.flavor_text_entries.map((c) => {
+            caracteristicas.flavor_text_entries.map((c, index) => {
               if (c.language.name === "en") {
                 return (
                   <Typography
-                    variant="p"
-                    component="li"
+                    variant="subtitle1"
+                    gutterBottom
                     display="block"
                     className={classes.descriptionTitle}
+                    key={`caract-${Date.now() - index}`}
                   >
                     {c.flavor_text}
                   </Typography>
@@ -104,26 +108,27 @@ function ViewPokemon({ pokemon }) {
             })}
           <Divider />
           <Typography
-            variant="h5"
-            component="h2"
+            variant="h3"
             display="block"
             className={classes.descriptionTitle}
           >
             Habilidades
           </Typography>
           {abilities.effect_entries !== undefined &&
-            abilities.effect_entries.map((h) => {
-              if(h.language.name === 'en')
-            return  <Typography
-                variant="p"
-                component="li"
-                display="block"
-                className={classes.descriptionTitle}
-              >
-                {h.effect}
-              </Typography>
-            })
-          }
+            abilities.effect_entries.map((h, index) => {
+              if (h.language.name === "en")
+                return (
+                  <Typography
+                    variant="subtitle1" 
+                    gutterBottom
+                    display="block"
+                    className={classes.descriptionTitle}
+                    key={`poder-${Date.now() - index}`}
+                  >
+                    {h.effect}
+                  </Typography>
+                );
+            })}
           <Divider />
         </Grid>
       </Grid>
@@ -131,6 +136,6 @@ function ViewPokemon({ pokemon }) {
   );
 }
 ViewPokemon.propTypes = {
-  pokemon: PropTypes.any
-}
+  pokemon: PropTypes.any,
+};
 export default ViewPokemon;
